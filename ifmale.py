17ad8015893aba26434028979e66e616b8d14610
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-12-03 08:20:53 -0800

    Add files via upload

---
+++ ifmale.py
@@ -0,0 +1,15 @@
+from asyncio.format_helpers import _format_callback_source
+
+
+is_male = True
+is_tall = False
+
+if is_male:
+    print("male")
+else:
+    print("female")
+
+if is_tall:
+    print("you are tall")
+else:
+    print("you are short")
